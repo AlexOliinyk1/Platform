c92d0a312882936465b92e086110a842c5c8ec02
Serhiy <serhiy.matvijuk@gmail.com>
2017-03-14 22:45:35 +0200

    Added resources data access layer

---
+++ Platform.API/Controllers/ContactsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Platform.API.Controllers
+{
+    public class ContactsController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return null;
+        }
+    }
+}
