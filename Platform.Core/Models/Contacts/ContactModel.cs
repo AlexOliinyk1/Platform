d49053a198948bf2e0d1d57ed54a17a11cb5d674
Serhiy <serhiy.matvijuk@gmail.com>
2017-03-14 22:25:49 +0200

    update view model

--- Platform.Core/Models/Contacts/ContactModel.cs
+++ Platform.Core/Models/Contacts/ContactModel.cs
@@ -17,30 +17,29 @@ namespace Platform.Core.Models.Contacts
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-        [Required]
         [Display(Name = "Title")]
         public string Title { get; set; }
 
-        [Required]
         [Display(Name = "Company")]
         public bool IsCompany { get; set; }
 
-        [Required]
         [Display(Name = "Type")]
         public ContactTypes ContatctType { get; set; }
 
-        [Required]
         [DataType(DataType.EmailAddress)]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
         [Display(Name = "Vat Number")]
         public int VatNumber { get; set; }
 
-        [Required]
         [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
+
+        public string Address{ get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Zip { get; set; }
 
     }
 }
