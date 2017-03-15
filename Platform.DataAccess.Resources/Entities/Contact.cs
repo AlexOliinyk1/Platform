469cec65b9d86db678dd328323df19e2fefacb6e
Serhiy <serhiy.matvijuk@gmail.com>
2017-03-16 00:55:26 +0200

    update entities

--- Platform.DataAccess.Resources/Entities/Contact.cs
+++ Platform.DataAccess.Resources/Entities/Contact.cs
@@ -1,7 +1,12 @@
-﻿namespace Platform.DataAccess.Resources.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Platform.DataAccess.Resources.Entities
 {
     public class Contact
     {
+        [Key]
+        public int Id { get; set; }
+
         public string Name { get; set; }
         
         public string Title { get; set; }
